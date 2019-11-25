9e09627bd61ecd53886fd3d41205de7c8dae78da
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-25 13:15:20 -0500

    met all specs

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export class App extends Component {
+
+    state = {
+        space: null,
+        loading: true
+    }
+
+    componentDidMount() {
+        // fetch
+        fetch("http://api.open-notify.org/astros.json")
+            .then(resp => resp.json())
+            .then(data => this.setState({
+                space: data,
+                loading: false
+            }))
+    }
+    
+    render() {
+        return (
+            <div>
+                {/* <-- stuff --/> */}
+                { !this.state.loading ? this.state.space.people.count : null }
+            </div>
+        );
+    }
+}
+
+export default App;
